429a0b18ed6c434a185c3153c0f1869c47f48165
payal603 <91979509+payal603@users.noreply.github.com>
2021-10-13 00:32:38 +0530

    Add files via upload
    This is an assignment assigned by the MyCaptain App.T his code is made by Payal Gaikwad

---
+++ Circle_Area.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 13 00:28:44 2021
+
+@author: payal gaikwad
+"""
+
+PI = 3.14
+r = float(input("Enter the radius of a circle:"))
+area = PI * r * r
+print("Area of a circle = %.11f" %area)
