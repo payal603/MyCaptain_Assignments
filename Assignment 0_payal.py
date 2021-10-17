6a9590563704f558e61d8d53e956db8e710cc4a3
payal603 <91979509+payal603@users.noreply.github.com>
2021-10-17 15:24:14 +0530

    Add files via upload

---
+++ Assignment 0_payal.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 17 14:07:22 2021
+
+@author: payal gaikwad
+"""
+#write algorith for finding x and y from below sequence 10,1,8,3,6,5,4,7,x,y
+x=10
+y=1
+while x>=0:
+    print(x,',',y,',',end="")
+    x=x-2
+    y=y+2
+    
+    
+ 
