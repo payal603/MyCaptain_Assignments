e5960023a0b783d8bef6ffe3abdbb471928acd9c
payal603 <91979509+payal603@users.noreply.github.com>
2021-11-15 21:03:33 +0530

    Update Hello_World.java

--- Hello_World.java
+++ Hello_World.java
@@ -1,9 +1,6 @@
 /******************************************************************************
 
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
+  Name:Payal Ravindra Gaikwad
 *******************************************************************************/
 
 public class Hello
