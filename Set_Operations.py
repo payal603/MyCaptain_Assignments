4fdc18fb028aae98ff11606ca518ea7922e1015d
payal603 <91979509+payal603@users.noreply.github.com>
2022-03-30 03:06:22 +0530

    Add files via upload
    This python code shows some basic set operations linke union,intersection.
    Created by Payal Ravindra Gaikwad

---
+++ Set_Operations.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 26 21:57:29 2022
+
+@author: dell
+"""
+
+
+# define three sets
+E = {0, 2, 4, 6, 8};
+N = {1, 2, 3, 4, 5};
+
+# set union
+print("Union of E and N is",E | N)
+
+# set intersection
+print("Intersection of E and N is",E & N)
+
+# set difference
+print("Difference of E and N is",E - N)
+
+# set symmetric difference
+print("Symmetric difference of E and N is",E ^ N)
